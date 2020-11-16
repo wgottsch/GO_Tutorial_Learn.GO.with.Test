b1686187c53d01b75ff1996dae1aefc7b42f2269
Wolfgang Gottschalk <wolfgang@the-gottschalks.de>
2020-11-16 16:18:37 +0100

    Test first

---
+++ Hello World/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//package GO_Tutorial_Learn_GO_with_Test
+
+func Hello(name string) string {
+	return "Hello " + name
+}
+func main() {
+	fmt.Println(Hello("Wolle"))
+
+}
