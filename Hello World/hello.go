b6507f90118cfa4ad8604f71244a345c6c0e1bb2
Wolfgang Gottschalk <wolfgang@the-gottschalks.de>
2020-11-16 16:42:24 +0100

    go mod init <module>

--- Hello World/hello.go
+++ Hello World/hello.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 //package GO_Tutorial_Learn_GO_with_Test
+const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
-	return "Hello " + name
+	return englishHelloPrefix + name
 }
 func main() {
 	fmt.Println(Hello("Wolle"))
